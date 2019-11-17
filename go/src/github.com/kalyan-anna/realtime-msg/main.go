7741cf392fa79a53a003b44767ccfa75ff9b4c78
Kalyan <kalyanasundaramannam@nbnco.com.au>
2019-11-18 07:40:25 +1030

    basic server setup

--- go/src/github.com/kalyan-anna/realtime-msg/main.go
+++ go/src/github.com/kalyan-anna/realtime-msg/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
+
+type Channel struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+}
 
 func main() {
-	fmt.Println("Hello from go")
+	router := NewRouter()
+	// router.Handle("channel add", addChannel)
+
+	http.Handle("/", router)
+	http.ListenAndServe(":4000", nil)
 }
+
